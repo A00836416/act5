350293aa34a70a4c7e9c5384f411d3112791255f
HarolDeveloper <hlancherosalvarez@gmail.com>
2025-05-12 19:48:01 -0600

    mult method added

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include "sum.h"
 #include "sub.h"
 #include "div.h"
+#include "mult.h"
 
 using std::cout;
 
-int main() {
+int main()
+{
 
     std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
     std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
     std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
+    std::cout << "mult(4, 5) = " << mult(4, 5) << '\n';
     return 0;
 }
